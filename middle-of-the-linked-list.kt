76d0d9b4b6efc7fb4ce74e245375b36919c94414
V.S. Sabarinath <152458209+vssabarinath@users.noreply.github.com>
2025-05-25 14:14:20 +0530

    Create middle-of-the-linked-list.kt

---
+++ middle-of-the-linked-list.kt
@@ -0,0 +1,13 @@
+class Solution {
+    fun middleNode(head: ListNode?): ListNode? {
+        var slow = head
+        var fast = head
+
+        while (fast?.next != null) {
+            slow = slow?.next
+            fast = fast.next.next
+        }
+
+        return slow
+    }
+}
